21f943aecb717052ddc5a71d564a827e4239e473
Razarx02 <specialforalem2mail.ru>
2019-10-16 21:46:02 +0600

    SDSADASD

--- isnegative.go
+++ isnegative.go
@@ -1,13 +1,13 @@
 package main
 
-import piscine ".."
+
 
 
 import "github.com/01-edu/z01"
 
-func Isnegative(nb int){
+func Isnegative(nb int) {
    
-	if nb >= 0{
+	if nb >= 0 {
 		z01.PrintRune('T')
 		z01.PrintRune(10)
 	} else {
