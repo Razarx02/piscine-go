7554c4f1519d03d14361a157a859f776b5a5bc60
Razarx02 <specialforalem2mail.ru>
2019-10-17 16:39:36 +0600

    ASDdaaaaaa

--- printcomb.go
+++ printcomb.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func PrintComb(i) {
 
-	  if i[0] <= i[1] && i[1] <= i[2] {
+	  if i[0] < i[1] && i[1] < i[2] {
 		  z01.PrintRune(rune(i[0]))  
 	  	  z01.PrintRune(rune(i[1]))
 		  z01.PrintRune(rune(i[2]))
