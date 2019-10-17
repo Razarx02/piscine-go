bc503e2fb2e48ca857403d11e209b35433d78817
Razarx02 <specialforalem2mail.ru>
2019-10-17 17:20:55 +0600

    ASDddddddddd

--- printcomb.go
+++ printcomb.go
@@ -2,14 +2,24 @@ package student
 
 import "github.com/01-edu/z01"
 
-func PrintComb(i) {
-
-	  if i[0] < i[1] && i[1] < i[2] {
-		  z01.PrintRune(rune(i[0]))  
-	  	  z01.PrintRune(rune(i[1]))
-		  z01.PrintRune(rune(i[2]))
-		  z01.PrintRune(',')
-		  z01.PrintRune(' ')
+func PrintComb() {
+	
+	for i := '0'; i <= '9'; i++ {
+		
+		for b := '0'; b <= '9'; b++ {
+			for z := '0'; z <= '9'; z++{
+		   		if i < b && b < z {
+			   		z01.PrintRune(rune(i))
+			   		z01.PrintRune(rune(b))
+					z01.PrintRune(rune(z))   
+					z01.PrintRune(',')
+			   		z01.PrintRune(' ')
+		   }
+		   }
+		}
 	} 
-
+	
+	
 }
+
+
