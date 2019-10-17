aacb264783e7a39a120727783c7e4e1eee28350b
Razarx02 <specialforalem2mail.ru>
2019-10-17 16:37:15 +0600

    ASDdaaaaaa

---
+++ printcomb.go
@@ -0,0 +1,15 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func PrintComb(i string) {
+
+	  if i[0] <= i[1] && i[1] <= i[2] {
+		  z01.PrintRune(rune(i[0]))  
+	  	  z01.PrintRune(rune(i[1]))
+		  z01.PrintRune(rune(i[2]))
+		  z01.PrintRune(',')
+		  z01.PrintRune(' ')
+	} 
+
+}
