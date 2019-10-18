66aa6945a25e7c7c597c161934f1e87857c0b6d4
Razarx02 <specialforalem2mail.ru>
2019-10-18 18:35:38 +0600

    ASDSDSD

--- printcomb2.go
+++ printcomb2.go
@@ -4,46 +4,37 @@ import "github.com/01-edu/z01"
 
 func PrintComb2() {
 
-	 
-	
-
-	for j := '0'; j <= '9'; j++{
+	for j := '0'; j <= '9'; j++ {
 		for i := '0'; i <= '9'; i++ {
 			for Da := j; Da <= '9'; Da++ {
-				
-				for Has := '0'  ; Has <= '9'; Has++ {
-					
-					if ((j != i) || (Da != Has) ) && ((j != Da ) || (i != Has))  {
-					
-					    if Has >= i {
-						z01.PrintRune(j)
-						z01.PrintRune(i)
-						z01.PrintRune(' ')
-						z01.PrintRune(Da)
-						z01.PrintRune(Has)
-							
-					    if j != '9' || i != '8' || Da != '9' || Has != '9' {
-							z01.PrintRune(',')
+
+				for Has := '0'; Has <= '9'; Has++ {
+
+					if ((j != i) || (Da != Has)) && ((j != Da) || (i != Has)) {
+
+						if Has >= i {
+							z01.PrintRune(j)
+							z01.PrintRune(i)
 							z01.PrintRune(' ')
-	
-						}   
-						} 
+							z01.PrintRune(Da)
+							z01.PrintRune(Has)
+
+							if j != '9' || i != '8' || Da != '9' || Has != '9' {
+								z01.PrintRune(',')
+								z01.PrintRune(' ')
+
+							}
 						}
-								  
-
-					} 
-                      
- 
+					}
 
 				}
-                     
 
-				  
-				}
-				
 			}
-		
-	
-		z01.PrintRune('\n')
-	
+
+		}
+
+	}
+
+	z01.PrintRune('\n')
+
 }
