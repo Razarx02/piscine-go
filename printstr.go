3345be5a49ee665b1b2f27267de580995d403682
Razarx02 <specialforalem2mail.ru>
2019-10-19 14:05:31 +0600

    WWWWWWWWWWWWWWWC

--- printstr.go
+++ printstr.go
@@ -3,12 +3,11 @@ package student
 import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
-	
+
 	a := len(str)
-		
-	for i := 0; i <= a - 1; i++ {
-		
-		
+
+	for i := 0; i <= a-1; i++ {
+
 		z01.PrintRune(rune(str[i]))
 
 	}
