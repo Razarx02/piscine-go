ec472ff07988b32b277bde8df860d4d6699166cf
Razarx02 <specialforalem2mail.ru>
2019-10-19 15:14:01 +0600

    To RANGEE

--- printstr.go
+++ printstr.go
@@ -4,11 +4,9 @@ import "github.com/01-edu/z01"
 
 func PrintStr(str string) {
 
-	a := z01.len(str)
+	for _, word := range str {
 
-	for i := 0; i <= a-1; i++ {
-
-		z01.PrintRune(rune(str[i]))
+		z01.PrintRune(word)
 
 	}
 
