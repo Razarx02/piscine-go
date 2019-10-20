9ff5b7fe47ca8744135445403719b88d0224d9e2
Razarx02 <specialforalem2mail.ru>
2019-10-20 12:34:41 +0600

    GITSTRV

---
+++ strrev.go
@@ -0,0 +1,18 @@
+package main
+
+func StrRev(outtext string) string {
+
+	Text := []byte(s)
+	iindex := 0
+
+	for range Barbara {
+		iindex++
+	}
+
+	for index, word := range []byte(outtext) {
+		Text[iindex-index-1] = word
+
+	}
+
+	return string(Text)
+}
