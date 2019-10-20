822427e290e0db10890f5318776852de376cba0e
Razarx02 <specialforalem2mail.ru>
2019-10-20 14:36:13 +0600

    quest3, basicatoi

--- basicatoi.go
+++ basicatoi.go
@@ -3,26 +3,24 @@ package student
 func BasicAtoi(word string) string {
 
 	toText := []byte(word)
-	index := 0
-
-	for range toText {
-
-		index++
-	}
-
-	if toText[index-1] == '0' {
-
-		return string('0')
-	}
+	Chek := true
 
 	for range []byte(word) {
 
 		if toText[0] == '0' {
 
 			toText = toText[1:]
+			Chek = false
 
+		} else {
+			Chek = true
 		}
 
+	}
+
+	if Chek == false {
+
+		return string('0')
 	}
 
 	return string(toText)
