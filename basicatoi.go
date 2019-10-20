97ef2c84a9b383d7862a03746129dcd41e066a2f
Razarx02 <specialforalem2mail.ru>
2019-10-20 15:09:50 +0600

    Quwest,3 basicatoi

--- basicatoi.go
+++ basicatoi.go
@@ -7,12 +7,12 @@ func BasicAtoi(word string) string {
 
 	for range []byte(word) {
 
-		if toText[0] == "0" {
+		if toText[0] == '0' {
 
 			toText = toText[1:]
 			Chek = false
 
-		} else if toText[0] == " " {
+		} else if toText[0] == ' ' {
 
 			Chek = false
 
@@ -36,5 +36,5 @@ func BasicAtoi(word string) string {
 
 	Finaltext := string(toText)
 
-	return string(Finaltext)
+	return Finaltext
 }
