f8caf57b30c9ae373a5c795e7acdfa2f82514785
Razarx02 <specialforalem2mail.ru>
2019-10-21 19:32:03 +0600

    Quest4, recursivfactorial

---
+++ recursivefactorial.go
@@ -0,0 +1,19 @@
+package student
+
+func RecursiveFactorial(Number int) int {
+
+	if Number == 0 {
+
+		return 1
+
+	} else if Number > 12 {
+
+		return 0
+
+	} else {
+
+		return Number * RecursiveFactorial(Number-1)
+
+	}
+
+}
