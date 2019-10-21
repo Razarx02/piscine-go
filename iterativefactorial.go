c439f68de201b48c95e15fab1b3c695754bed617
Razarx02 <specialforalem2mail.ru>
2019-10-21 19:05:20 +0600

    Quest4, iterativefactorial1111111

---
+++ iterativefactorial.go
@@ -0,0 +1,25 @@
+package student
+
+func IterativeFactorial(s int) int {	 
+	
+	result := 1
+	
+	if s == 0 {
+
+		return 1
+		
+	 }else if s < 0{
+		 
+		return
+	  }
+
+     
+	for i := 1; i < s+1; i++ {
+  
+		 result = result*i		
+	} 
+	
+	return result
+
+}
+
