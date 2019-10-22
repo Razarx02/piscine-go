62de173a281aeee0d2a634a5f735ab95a11b697f
Razarx02 <specialforalem2mail.ru>
2019-10-22 09:52:22 +0600

    Quest5, alphacount

--- alphacount.go
+++ alphacount.go
@@ -5,7 +5,7 @@ func AlphaCount(str string) int {
 	A := []rune(str)
 	count := 0
 
-	for index, _ := range A {
+	for index := range A {
 
 		if (A[index] > 96 && 123 > A[index]) || (A[index] > 64 && 91 > A[index]) {
 
