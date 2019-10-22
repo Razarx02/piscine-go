16bc61dc0b1018ac9b5d074801f8af00e0db5c0c
Razarx02 <specialforalem2mail.ru>
2019-10-22 09:50:51 +0600

    Quest5, alphacount

---
+++ alphacount.go
@@ -0,0 +1,18 @@
+package student
+
+func AlphaCount(str string) int {
+
+	A := []rune(str)
+	count := 0
+
+	for index, _ := range A {
+
+		if (A[index] > 96 && 123 > A[index]) || (A[index] > 64 && 91 > A[index]) {
+
+			count++
+
+		}
+	}
+
+	return count
+}
