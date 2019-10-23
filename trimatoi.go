c46caec9857ae79c8686891ba62e1378f13757ec
Razarx02 <specialforalem2mail.ru>
2019-10-23 17:19:39 +0600

    quest5, 46545

---
+++ trimatoi.go
@@ -0,0 +1,25 @@
+package student
+
+func TrimAtoi(s string) int {
+
+	Nomer := 0
+	Chekminus := 1
+
+	for _, i := range s {
+
+		if i >= '0' && i <= '9' {
+
+			N := int(i - 48)
+
+			Nomer = T*10 + N
+
+		} else if i == '-' && Nomer == 0 {
+
+			Chekminus = -1
+
+		}
+
+	}
+
+	return Nomer * Chekminus
+}
