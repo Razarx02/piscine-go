fde15284dfcee53a377196f5db15d336bd1c4ff3
Razarx02 <specialforalem2mail.ru>
2019-10-23 18:16:52 +0600

    Quest5, Runs

--- firstrune.go
+++ firstrune.go
@@ -2,17 +2,7 @@ package student
 
 func FirstRune(s string) rune {
 
-	W := 0
-
-	for i := -50; i <= 300; i++ {
-
-		if int(s[0]) == i {
-
-			W = i
-
-		}
-
-	}
+	W := int(s[0])
 
 	return rune(W)
 }
