c42f38c1df549060e9d5ccf20838b78f4eb0c805
Razarx02 <specialforalem2mail.ru>
2019-10-23 18:06:53 +0600

    Quest5, Runs

--- firstrune.go
+++ firstrune.go
@@ -5,18 +5,20 @@ func FirstRune(s string) rune {
 	W := 0
 
 	if int(s[0]) > 90 || int(s[0]) < 65 {
+
 		return 0
-	}
 
-	for i := 65; i <= 90; i++ {
+	} else {
 
-		if int(s[0]) == i {
+		for i := 65; i <= 90; i++ {
 
-			W = i
+			if int(s[0]) == i {
+
+				W = i
+
+			}
 
 		}
-
 	}
-
 	return rune(W)
 }
