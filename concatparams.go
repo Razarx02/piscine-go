7ac61c195c7d24c5100cc3d322c6e1674ea3b18b
Razarx02 <specialforalem2mail.ru>
2019-10-24 23:05:37 +0600

    quiwwwwww

---
+++ concatparams.go
@@ -0,0 +1,23 @@
+package student
+
+func ConcatParams(args []string) string {
+
+	lol := 0
+
+	for range args {
+
+		lol++
+	}
+
+	var str string
+	for i := 0; i < lol; i++ {
+
+		str = str + string(args[i])
+		if i != lol-1 {
+			str = str + "\n"
+		}
+
+	}
+
+	return str
+}
