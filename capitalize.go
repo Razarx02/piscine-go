77dc7c967c0e4075d30d2bca5562554e6571ec39
Razarx02 <specialforalem2mail.ru>
2019-10-24 19:17:43 +0600

    QQQQQQQQQQQQQQQQQQQ

--- capitalize.go
+++ capitalize.go
@@ -1,19 +1,26 @@
 package student
 
 func Capitalize(s string) string {
-
-	A := []rune(s)
-
-	for i := range s {
-
-		if (A[i] == 32) && (A[i+1] > 96 && A[i+1] < 123) {
-
-			A[i+1] = A[i+1] - rune(32)
-
+	y := 0
+	new := []rune(s)
+	for range s {
+		y++
+	}
+	for i := 0; i < y; i++ {
+		if new[i] >= 'A' && new[i] <= 'Z' {
+			new[i] = (new[i] + 32)
+		}
+		if new[0] >= 'a' && new[0] <= 'z' {
+			new[0] = (new[0] - 32)
+		}
+		if i > 0 {
+			if (new[i-1] > 'Z' && new[i-1] < 'a') || (new[i-1] < 'A' && new[i-1] > '9') || new[i-1] < '0' || new[i-1] > 'z' {
+				if new[i] >= 'a' && new[i] <= 'z' {
+					new[i] = (new[i] - 32)
+				}
+			}
 		}
 	}
-
-	text := string(A)
-
-	return text
+	str := string(new)
+	return str
 }
