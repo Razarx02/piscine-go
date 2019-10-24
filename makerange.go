c1cda2835de76d8255d2bfac04386cd7a5d0e2c9
Razarx02 <specialforalem2mail.ru>
2019-10-24 22:44:04 +0600

    oooooooooooooo

---
+++ makerange.go
@@ -0,0 +1,23 @@
+package student
+
+func AppendRange(min int, max int) []int {
+
+	var forz []int = make([]int, 0)
+
+	if min == max || min > max {
+		return forz
+	}
+	sred := max - min
+	var Buffer []int = make([]int, sred)
+	index := 0
+
+	for i := min; i < max; i++ {
+
+		Buffer[index] = i
+		index++
+
+	}
+
+	return Buffer
+
+}
