e787438e8b31c1715e9183b6e9b609f54a58a914
Razarx02 <specialforalem2mail.ru>
2019-10-24 17:43:47 +0600

    QQQQQQQQQQQ

---
+++ compare.go
@@ -0,0 +1,30 @@
+package student
+
+func Compare(a, b string) int {
+	Cat := 0
+	Dog := 0
+	
+	for range a {
+       Cat++  
+	}
+	for range b {
+		Dog++
+	}
+
+	Z := Cat - Dog
+
+	if b == a {
+	   
+		return 0
+
+	} else if b == a[Z:]  {
+
+		return -1
+
+		}  else {
+
+		return 1
+	} 
+	
+
+}
